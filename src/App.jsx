fa5c92b880cb8f1c6662640ed0e8b76e2bbfd029
Tracy-miranja <barazatracy44@gmail.com>
2024-10-17 00:12:55 +0300

    fixed a bag

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import Dashboard from './Dashboard';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Overview from './overview';
-import Form from './LoginForm';
+import Dashboard from "./Dashboard";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Overview from "./overview";
+import Form from "./LoginForm";
+import SignUpForm from "./signup";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Form />} /> 
-        <Route path="/Dashboard" element={<Dashboard />} /> 
-        <Route path="/overview" element={<Overview />} /> 
+        <Route path="/" element={<Form />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/overview" element={<Overview />} />
       </Routes>
     </Router>
   );
